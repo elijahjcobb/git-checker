d84bf817af5c9bce351feac6779928573aedb9fb
Elijah Cobb <elijah.cobb@vercel.com>
2022-04-12 20:15:16 -0400

    added new logging

--- git-checker.ts
+++ git-checker.ts
@@ -5,9 +5,18 @@
  * github.com/elijahjcobb
  */
 
-import { italic, bold, red, green } from "https://deno.land/std/fmt/colors.ts";
+import { italic, bold, red, green } from "https://deno.land/std@0.134.0/fmt/colors.ts";
 
 const cwd = Deno.cwd();
+
+type Repo = {
+	safe: boolean;
+	name: string;
+	status: string;
+	emoji: string;
+}
+
+const repos: Repo[] = [];
 
 for (const path of Deno.readDirSync(cwd)) {
 
@@ -29,23 +38,49 @@ for (const path of Deno.readDirSync(cwd)) {
 	const msg = new TextDecoder("utf8").decode(stdout);
 
 	const possibleMessages = {
-		"to be committed": "has staged changes that have not been committed",
-		"not staged for commit": "has changes that have not been staged",
-		"Untracked files": "has un-tracked files that have not been ignored",
+		"to be committed": "needs to be committed",
+		"not staged for commit": "has un-staged changes",
+		"Untracked files": "has un-tracked files",
 		"is ahead of": "needs to be pushed"
 	}
 
+	const emojis = {
+		"to be committed": "✍️",
+		"not staged for commit": "🗂",
+		"Untracked files": "🤯",
+		"is ahead of": "☁️",
+	}
+
 	let ret: string | undefined = undefined;
+	let emoji: string | undefined;
 
 	for (const posMsg in possibleMessages) {
 		if (msg.includes(posMsg)) {
-			// @ts-ignore
+			// @ts-ignore ignore type index on string
 			ret = possibleMessages[posMsg];
+			// @ts-ignore ignore type index on string
+			emoji = emojis[posMsg];
 			break;
 		}
 	}
 
-	console.log(italic(path.name) + ": " + bold(ret === undefined ? green("clean!") : red(ret + ".")));
+	repos.push({
+		safe: ret === undefined,
+		status: ret ?? "",
+		name: path.name,
+		emoji: emoji ?? "🔥"
+	})
 
 }
 
+
+const printRepo = (repo: Repo) => {
+	console.log((repo.safe ? "✅" : "❌") + " " + repo.emoji + " " + bold(repo.name) + (repo.safe ? green : red)(" " + repo.status));
+}
+
+const sorter = (a: Repo, b: Repo): number => {
+	return a.name.localeCompare(b.name);
+}
+
+repos.filter(r => r.safe).sort(sorter).forEach(printRepo);
+repos.filter(r => !r.safe).sort(sorter).forEach(printRepo);
